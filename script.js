250224a294d83a39508f111a1e1c4e699266e121
Durgesh Sharma <109064132+Durge15@users.noreply.github.com>
2023-10-16 16:06:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
-const button=document.getElementById("enterBtn");
-const para=document.getElementById("status");
+const button=document.getElementById("#enterBtn");
+const para=document.getElementById("#status");
 
 button.addEventListener("click",() =>{
 	para.innerHTML="<H1>Entered Metaverse</H1>";
